ed57f17ce877f5cb07694c257dcc8058b4163fdb
Tonyreng <tonyreng01@gmail.com>
2025-05-08 13:32:24 +0000

    Creando componente Cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
 import { Header } from "../components/Header.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Cards } from "../components/Cards.jsx";
 
 export const Home = () => {
   const { store, dispatch } = useGlobalReducer();
 
-  return <Header />;
+  const peopleApi = () => {
+    fetch("https://www.swapi.tech/api/people")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({
+          type: "set_Characters",
+          payload: data.results,
+        })
+      )
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    peopleApi();
+  }, []);
+
+  let characters = store.characters;
+
+  return (
+    <>
+      <Header />
+      <Cards value={characters} />
+  </>)
 };
--- src/store.js
+++ src/store.js
@@ -12,7 +12,8 @@ export const initialStore=()=>{
         title: "Do my homework",
         background: null,
       }
-    ]
+    ],
+    characters: [],
   }
 }
 
@@ -26,6 +27,11 @@ export default function storeReducer(store, action = {}) {
         ...store,
         todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
       };
+    case 'set_Characters': 
+      return {
+        ...store,
+        characters: action.payload,
+      }
     default:
       throw Error('Unknown action.');
   }    
